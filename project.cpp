ff85389ec1f38164fe1421ece503fb27d934494b
TommyCesard <49964547+TommyCesard@users.noreply.github.com>
2020-07-16 19:04:32 -0400

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -26,6 +26,8 @@ struct process {
 	bool done = false;
 	char id; // a to z
 	
+	bool preempt = false;
+
 	int arr_time; // determined at the beginning in preemption
 	int burst_time;
 	int rem_burst_time;	
@@ -43,6 +45,7 @@ struct process {
 	
 	std::vector<int> cpu_bursts;
 	std::vector<int> io_bursts;
+	std::vector<int> turnaround_times;
 
 	bool operator<(const process& p)
 	{
@@ -104,7 +107,7 @@ void printq(std::vector<process*> q)
 
 //Runs the FCFS CPU scheduling algorithm and returns a list containing
 //[average wait time time, average turnaround time, # of context switches, # of preemptions]
-std::vector<int> FCFS(std::map<char, process*> proc_map, std::vector<process*> procs, int t_cs)
+std::vector<double> FCFS(std::map<char, process*> proc_map, std::vector<process*> procs, int t_cs)
 {
 	for(int i = 0; i < procs.size(); i++)
 	{
@@ -114,13 +117,13 @@ std::vector<int> FCFS(std::map<char, process*> proc_map, std::vector<process*> p
 	/*
 		do shit
 	*/
-	std::vector<int> temp = {0, 0, 0, 0};
+	std::vector<double> temp = {0, 0, 0, 0};
 	return temp;
 }
 
 //Runs the SJF CPU scheduling algorithm and returns a list containing
 //[average wait time time, average turnaround time, # of context switches, # of preemptions]
-std::vector<int> SJF(std::map<char, process*> proc_map, std::vector<process*> procs, int t_cs)
+std::vector<double> SJF(std::map<char, process*> proc_map, std::vector<process*> procs, int t_cs)
 {
 	for(int i = 0; i < procs.size(); i++)
 	{
@@ -130,13 +133,13 @@ std::vector<int> SJF(std::map<char, process*> proc_map, std::vector<process*> pr
 	/*
 		do shit
 	*/
-	std::vector<int> temp = {0, 0, 0, 0};
+	std::vector<double> temp = {0, 0, 0, 0};
 	return temp;
 }
 
 //Runs the SRT CPU scheduling algorithm and returns a list containing
 //[average wait time time, average turnaround time, # of context switches, # of preemptions]
-std::vector<int> SRT(std::map<char, process*> proc_map, std::vector<process*> procs, int t_cs)
+std::vector<double> SRT(std::map<char, process*> proc_map, std::vector<process*> procs, int t_cs)
 {
 	for(int i = 0; i < procs.size(); i++)
 	{
@@ -146,13 +149,13 @@ std::vector<int> SRT(std::map<char, process*> proc_map, std::vector<process*> pr
 	/*
 		do shit
 	*/
-	std::vector<int> temp = {0, 0, 0, 0};
+	std::vector<double> temp = {0, 0, 0, 0};
 	return temp;
 }
 
 //Runs the RR CPU scheduling algorithm and returns a list containing
-//[average wait time time, average turnaround time, # of context switches, # of preemptions]
-std::vector<int> RR(std::map<char, process*> proc_map, std::vector<process*> procs, int t_cs, int t_slice, std::string rr_add)
+//[wait time time, average turnaround time, # of context switches, # of preemptions]
+std::vector<double> RR(std::map<char, process*> proc_map, std::vector<process*> procs, int t_cs, int t_slice, std::string rr_add, std::string alg)
 {
 /*##################################################################*/
 	bool full_output = true;
@@ -161,8 +164,8 @@ std::vector<int> RR(std::map<char, process*> proc_map, std::vector<process*> pro
 	{
 		std::cout << "Process " << procs[i]->id << " [NEW] (arrival time " << procs[i]->arr_time << " ms) " << procs[i]->n_bursts << " CPU bursts\n";
 	}
-	std::cout << "time 0ms: Simulator started for RR [Q <empty>]\n";
-	std::vector<int> data = {0, 0, 0, 0};
+	std::cout << "time 0ms: Simulator started for " << alg << " [Q <empty>]\n";
+	std::vector<double> data = {0, 0, 0, 0};
 	int time = 0;
 	int rem_slice_time;
 	int total_cs = 0;
@@ -196,7 +199,7 @@ std::vector<int> RR(std::map<char, process*> proc_map, std::vector<process*> pro
 					{
 						if(full_output || time < 1000)
 						{
-							std::cout << "time " << time << "ms: Process " << cpu->curr_proc->id << " completed a CPU burst; " << cpu->curr_proc->rem_bursts << " to go ";
+							std::cout << "time " << time << "ms: Process " << cpu->curr_proc->id << " completed a CPU burst; " << cpu->curr_proc->rem_bursts << " burst to go ";
 							printq(readyQ);
 						}
 					}
@@ -213,6 +216,9 @@ std::vector<int> RR(std::map<char, process*> proc_map, std::vector<process*> pro
 						cpu->curr_proc->rem_burst_time = t_cs/2 + cpu->curr_proc->io_bursts[cpu->curr_proc->io_index] + 1;
 						cpu->curr_proc->io_index++;
 					}
+					//std::cout << "TURNAROUND TIME: Process " << cpu->curr_proc->id << " started at " << cpu->curr_proc->turnaround_times[cpu->curr_proc->cpu_index]
+					//<< "ms ended at " << time << "ms\n";
+					cpu->curr_proc->turnaround_times[cpu->curr_proc->cpu_index] = time + t_cs - cpu->curr_proc->turnaround_times[cpu->curr_proc->cpu_index];
 					cpu->curr_proc->cpu_index++;
 					cpu->rem_burst_time = 0;
 					cpu->switch_in_time = 0;
@@ -220,11 +226,12 @@ std::vector<int> RR(std::map<char, process*> proc_map, std::vector<process*> pro
 				}
 				rem_slice_time -= 1;
 				//Check if slice time ran out
-				if(rem_slice_time == 0)
+				if(rem_slice_time == 0 && cpu->rem_burst_time > 0)
 				{
 					//If there is a process to actuall preempt the CPU process
 					if(readyQ.size() > 0)
 					{
+						cpu->curr_proc->preempt = true;
 						if(full_output || time < 1000)
 						{
 							std::cout << "time " << time << "ms: Time slice expired; process " << cpu->curr_proc->id << " preempted with "
@@ -233,14 +240,6 @@ std::vector<int> RR(std::map<char, process*> proc_map, std::vector<process*> pro
 						}
 						total_preemptions += 1;
 						cpu->curr_proc->cpu_bursts[cpu->curr_proc->cpu_index] = cpu->rem_burst_time;
-						if(!rr_add.compare("BEGINNING"))
-						{
-							readyQ.insert(readyQ.begin(), cpu->curr_proc);
-						}
-						else
-						{
-							readyQ.push_back(cpu->curr_proc);
-						}
 						cpu->rem_burst_time = 0;
 						cpu->switch_in_time = 0;
 						cpu->switch_out_time = t_cs/2;
@@ -271,6 +270,10 @@ std::vector<int> RR(std::map<char, process*> proc_map, std::vector<process*> pro
 					total_cs += 1;
 					cpu->rem_burst_time = cpu->curr_proc->cpu_bursts[cpu->curr_proc->cpu_index];
 					rem_slice_time = t_slice;
+					if(cpu->curr_proc->turnaround_times[cpu->curr_proc->cpu_index] == 0)
+					{
+						cpu->curr_proc->turnaround_times[cpu->curr_proc->cpu_index] = time;
+					}
 				}
 			}
 			//If a process is currently being switched out of the CPU
@@ -279,6 +282,18 @@ std::vector<int> RR(std::map<char, process*> proc_map, std::vector<process*> pro
 				cpu->switch_out_time -= 1;
 				if(cpu->switch_out_time == 0)
 				{
+					if(cpu->curr_proc->preempt)
+					{
+						cpu->curr_proc->preempt = false;
+						if(!rr_add.compare("BEGINNING"))
+						{
+							readyQ.insert(readyQ.begin(), cpu->curr_proc);
+						}
+						else
+						{
+							readyQ.push_back(cpu->curr_proc);
+						}
+					}
 					cpu->curr_proc = NULL;
 				}
 			}
@@ -333,6 +348,7 @@ std::vector<int> RR(std::map<char, process*> proc_map, std::vector<process*> pro
 						std::cout << "time " << time << "ms: Process " << procs[i]->id  << " completed I/O; added to ready queue ";
 						printq(readyQ);
 					}
+					//procs[i]->wait_time += t_cs/2;
 				}
 			}
 		}
@@ -353,22 +369,44 @@ std::vector<int> RR(std::map<char, process*> proc_map, std::vector<process*> pro
 		time++;
 	}
 	time++;
-	std::cout << "time " << time << "ms: Simulator ended for RR [Q <empty>]\n";
+	std::cout << "time " << time << "ms: Simulator ended for " << alg << " [Q <empty>]\n";
+	int total_wait_time = 0;
+	int num_cpu_bursts = 0;
+	int total_turnaround_time = 0;
+	for(int i = 0; i < procs.size(); i++)
+	{
+		total_wait_time += procs[i]->wait_time;
+		num_cpu_bursts += procs[i]->cpu_bursts.size();
+		for(int j = 0; j < procs[i]->cpu_bursts.size(); j++)
+		{
+			total_turnaround_time += procs[i]->turnaround_times[j];
+		}
+	}
+	data[0] = (double)total_wait_time/num_cpu_bursts;
+	data[1] = (double)total_turnaround_time/num_cpu_bursts;
+	data[2] = (double)total_cs;
+	data[3] = (double)total_preemptions;	
 	return data;
 }
 
-//Prints stats to the required output file
-void print_stats(std::string alg, std::vector<int> data, FILE* out)
+//Takes in vector containing [average wait time time, average turnaround time, # of context switches, # of preemptions]
+//and prints it to the out FILE in the desired format
+void print_stats(char alg[], double ave_cpu_bt, std::vector<double> data, FILE* out)
 {
-	/*
-		print shit
-	*/
+	double x = 0;
+	fprintf(out, "Algorithm  %s\n-- average CPU burst time : %.3f ms\n", alg, ave_cpu_bt);
+	fprintf(out, "-- average wait time: %.3f ms\n", data[0]);
+	fprintf(out, "-- average turnaround time: %.3f ms\n", data[1]);
+	fprintf(out, "-- total number of context switches: %d\n", (int)data[2]);
+	fprintf(out, "-- total number of preemptions: %d\n", (int)data[3]);
 }
 
-void start_sim(int n_procs, int seed, double lambda, int upper, int t_cs, double alpha, int t_slice, FILE* out, std::string rr_add)
+void start_sim(int n_procs, int seed, double lambda, int upper, int t_cs, double alpha, int t_slice, FILE* out, std::string rr_add, std::string algorithm)
 {
 	std::map<char, process*> proc_map; // letter->process with letter
 	std::vector<process*> procs; // all the processes
+	int sum_cpu_bt = 0;
+	int num_cpu_b = 0;
 	srand48(seed);
 	for(char c='A'; c < ((char) 'A'+n_procs); c++)
 	{	
@@ -387,11 +425,14 @@ void start_sim(int n_procs, int seed, double lambda, int upper, int t_cs, double
 
 		proc->cpu_bursts = std::vector<int>(proc->n_bursts, 0);
 		proc->io_bursts = std::vector<int>(proc->n_bursts-1, 0);
+		proc->turnaround_times = std::vector<int>(proc->n_bursts, 0);
 
 		double sum = 0;
 		for(int i = 0; i < proc->n_bursts; i++)
 		{
 			proc->cpu_bursts[i] = ceil(exp_random(lambda, upper));
+			sum_cpu_bt += proc->cpu_bursts[i];
+			num_cpu_b++;
 			if(i != proc->n_bursts-1) proc->io_bursts[i] = ceil(exp_random(lambda, upper));
 		}
 		proc_map.insert(std::pair<char, process*>(c, proc));
@@ -401,17 +442,32 @@ void start_sim(int n_procs, int seed, double lambda, int upper, int t_cs, double
 
 	std::sort(procs.begin(), procs.end());	
 
-	std::vector<int> data;
-	/*
-	data = FCFS(proc_map, procs, t_cs);
-	print_stats("FCFS", data, out);
-	data = SJF(proc_map, procs, t_cs);
-	print_stats("SJF", data, out);
-	data = SRT(proc_map, procs, t_cs);
-	print_stats("SRT", data, out);
-	*/
-	data = RR(proc_map, procs, t_cs, t_slice, rr_add);
-	print_stats("RR", data, out);
+	//Calculates average CPU burst time
+	double ave_cpu_bt = (double)sum_cpu_bt/num_cpu_b;
+	std::vector<double> data;
+	
+	if(algorithm == "FCFS")
+	{
+		data = RR(proc_map, procs, t_cs, 9999999, "END", "FCFS");
+		char alg1[] = {'F', 'C', 'F', 'S'};
+		print_stats(alg1, ave_cpu_bt, data, out);
+		std::cout << "\n";
+	}
+		/*
+		data = SJF(proc_map, procs, t_cs);
+		char alg2[] = {'S', 'J', 'F'};
+		print_stats(alg2, ave_cpu_bt, data, out);
+
+		data = SRT(proc_map, procs, t_cs);
+		char alg3[] = {'S', 'R', 'T'};
+		print_stats(alg3, ave_cpu_bt, data, out);
+		*/
+	else
+	{
+		data = RR(proc_map, procs, t_cs, t_slice, rr_add, "RR");
+		char alg4[] = {'R', 'R'};
+		print_stats(alg4, ave_cpu_bt, data, out);
+	}
 }	
 
 int main(int argc, char* argv[])
@@ -433,11 +489,17 @@ int main(int argc, char* argv[])
 	int alpha = atoi(argv[6]);
 	int t_slice = atoi(argv[7]);
 	out = fopen("simout.txt", "w");
+	if(out == NULL)
+	{
+		std::cerr << "ERROR: could not open file\n";
+		return EXIT_FAILURE;
+	}
 	std::string rr_add = "END";
 	if(argc == 9 && strcmp(argv[8], "BEGINNING") == 0)
-		rr_add = "BEGINNING";	
-	start_sim(n, seed, lambda, upper, t_cs, alpha, t_slice, out, rr_add);
+		rr_add = "BEGINNING";
+	start_sim(n, seed, lambda, upper, t_cs, alpha, t_slice, out, rr_add, "FCFS");
+	start_sim(n, seed, lambda, upper, t_cs, alpha, t_slice, out, rr_add, "RR");
 
-
+	fclose(out);
 	return EXIT_SUCCESS;
 }
